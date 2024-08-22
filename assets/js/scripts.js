ae3857a1a3c143536e2b4ace98dfacba485850e0
Fernando Viveros <fernandoviveros95@gmail.com>
2024-08-22 11:10:06 -0300

    Agregado de funcionalidad y responsive

---
+++ assets/js/scripts.js
@@ -0,0 +1,74 @@
+const btnEncriptar = document.getElementById("btn-encriptar");
+const btnDesencriptar = document.getElementById("btn-desencriptar");
+
+const analisisEncrip = (letra) => {
+    let cambio = letra;
+    if (letra == "a") cambio = "ai";
+    if (letra == "e") cambio = "enter";
+    if (letra == "i") cambio = "imes";
+    if (letra == "o") cambio = "ober";
+    if (letra == "u") cambio = "ufat";
+    return cambio;
+}
+
+const analisisDecrip = (texto) => {
+    texto = texto.replaceAll("ai", "a");
+    texto = texto.replaceAll("enter", "e");
+    texto = texto.replaceAll("imes", "i");
+    texto = texto.replaceAll("ober", "o");
+    texto = texto.replaceAll("ufat", "u");
+    return texto;
+}
+
+const copiar = () => {
+    let texto = document.getElementById('texto').innerHTML;
+    const copiarContenido = async () => {
+        try {
+            await navigator.clipboard.writeText(texto);
+            alert('Contenido copiado al portapapeles');
+        } catch (err) {
+            console.error('Error al copiar: ', err);
+        }
+    }
+
+    copiarContenido();
+}
+
+const encriptar = () => {
+    const textoUsuario = document.getElementById("texto-usuario").value;
+    const contenedorResultado = document.getElementById("resultado-texto");
+    let textoEncriptado = "";
+
+    const iterator = textoUsuario[Symbol.iterator]();
+    let caracter = iterator.next();
+
+    while (!caracter.done) {
+        textoEncriptado += analisisEncrip(caracter.value);
+        console.log(textoEncriptado);
+        caracter = iterator.next();
+    }
+
+    contenedorResultado.innerHTML = `
+    <p class="texto" id="texto">${textoEncriptado}</p>
+<button class="btn secundario" id="btn-copiar">Copiar</button>
+    `
+
+    const btnCopiar = document.getElementById("btn-copiar");
+    btnCopiar.addEventListener('click', copiar);
+}
+
+const desencriptar = () => {
+    const textoUsuario = document.getElementById("texto-usuario").value;
+    const contenedorResultado = document.getElementById("resultado-texto");
+    let textoEncriptado = analisisDecrip(textoUsuario);
+
+    contenedorResultado.innerHTML = `
+    <p class="texto" id="texto">${textoEncriptado}</p>
+    <button class="btn secundario" id="btn-copiar">Copiar</button>
+    `
+    const btnCopiar = document.getElementById("btn-copiar");
+    btnCopiar.addEventListener('click', copiar);
+}
+
+btnEncriptar.addEventListener('click', encriptar);
+btnDesencriptar.addEventListener('click', desencriptar);
